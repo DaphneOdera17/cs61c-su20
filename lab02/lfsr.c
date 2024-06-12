1b931478079925663aed6e5d32cb62081bf66c8e
DaphneOdera17 <2944006086@qq.com>
2024-06-12 21:38:36 +0800

    finish lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,12 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    int n0, n2, n3, n5;
+    n0 = (*reg) & 1;
+    n2 = (*reg >> 2) & 1;
+    n3 = (*reg >> 3) & 1;
+    n5 = (*reg >> 5) & 1;
+    int MSB = n0 ^ n2 ^ n3 ^ n5;
+    (*reg) = (*reg >> 1) | (MSB << 15);
 }
 
